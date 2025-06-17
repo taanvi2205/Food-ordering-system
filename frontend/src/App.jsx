e458abb859d75e29016c998ee2c3de47a90d23b1
Taanvi Khevaria <taanvikhevaria@gmail.com>
2025-06-17 20:51:57 +0530

    Initial files setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import Cart from './pages/Cart/Cart'
+import Checkout from './pages/Checkout/Checkout'
+import Order from './pages/Order/Order'
+import Footer from './components/Footer/Footer'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element= {<Home/>}/>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/checkout' element={<Checkout/>}/>
+        <Route path='/orders' element={<Order/>}/>
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App 
---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,188 @@
+import food1 from './chicken_biryani.jpg';
+import food2 from './chicken_honeyChilli.jpg';
+import food3 from './chicken_salad.jpg';
+import food4 from './chicken_skewers.jpg';
+import food5 from './drink1.jpg';
+import food6 from './drink2.jpg';
+import food7 from './drink3.jpg';
+import food8 from './pasta_chilliOil.jpg';
+import food9 from './pasta_redSauce.jpg';
+import food10 from './pasta_whiteSauce.jpg';
+import food11 from './pizza1.jpg';
+import food12 from './pizza2.jpg';
+import food13 from './pizza3.jpg';
+import food15 from './rice1.jpg';
+import food16 from './rice2.jpg';
+import food17 from './rice3.jpg';
+
+
+export const categoryItem = [
+    {
+        category_title: "All"
+    },
+    {
+        category_title: "Chicken"
+    },
+    {
+        category_title: "Pizza"
+    },
+    {
+        category_title: "Rice"
+    },
+    {
+        category_title: "Drinks"
+    },
+    {
+        category_title: "Pasta"
+    }
+];
+
+
+export const product = [
+    {
+        _id: "a",
+        name: "Chicken Biryani",
+        description: "Spiced basmati rice with marinated chicken.",
+        price: 180,
+        image: food1,
+        category: "Chicken",
+        date: 1716634345448
+    },
+    {
+        _id: "b",
+        name: "Honey Chilli Chicken",
+        description: "Crispy chicken tossed in sweet-spicy honey chilli sauce.",
+        price: 170,
+        image: food2,
+        category: "Chicken",
+        date: 1716634345448
+    },
+    {
+        _id: "c",
+        name: "Grilled Chicken Salad",
+        description: "Healthy salad with grilled chicken and vinaigrette.",
+        price: 120,
+        image: food3,
+        category: "Chicken",
+        date: 1716634345448
+    },
+    {
+        _id: "d",
+        name: "Tandoori Chicken Skewers",
+        description: "Grilled chicken skewers marinated in Indian spices.",
+        price: 160,
+        image: food4,
+        category: "Chicken",
+        date: 1716634345448
+    },
+    {
+        _id: "e",
+        name: "Strawberry Mojito",
+        description: "Refreshing strawberry mojito with mint and lime.",
+        price: 65,
+        image: food5,
+        category: "Drink",
+        date: 1716634345448
+    },
+    {
+        _id: "f",
+        name: "Lemon Mojito",
+        description: "Zesty mint mojito with lemon and soda.",
+        price: 55,
+        image: food6,
+        category: "Drink",
+        date: 1716634345448
+    },
+    {
+        _id: "g",
+        name: "Orange Juice",
+        description: "Freshly squeezed orange juice.",
+        price: 50,
+        image: food7,
+        category: "Drink",
+        date: 1716634345448
+    },
+
+    {
+        _id: "h",
+        name: "Chilli Oil Spaghetti",
+        description: "Spaghetti tossed in spicy chilli oil and garlic.",
+        price: 130,
+        image: food8,
+        category: "Pasta",
+        date: 1716634345448
+    },
+    {
+        _id: "i",
+        name: "Red Sauce Pasta",
+        description: "Classic penne in tomato and herb sauce.",
+        price: 120,
+        image: food9,
+        category: "Pasta",
+        date: 1716634345448
+    },
+    {
+        _id: "j",
+        name: "White Sauce Pasta",
+        description: "Creamy white sauce pasta with cheese and veggies.",
+        price: 130,
+        image: food10,
+        category: "Pasta",
+        date: 1716634345448
+    },
+    {
+        _id: "k",
+        name: "Cheese Burst Pizza",
+        description: "Loaded with cheese and classic toppings.",
+        price: 150,
+        image: food11,
+        category: "Pizza",
+        date: 1716634345448
+    },
+    {
+        _id: "l",
+        name: "Pepperoni Pizza",
+        description: "Spicy pepperoni slices on mozzarella and tomato base.",
+        price: 160,
+        image: food12,
+        category: "Pizza",
+        date: 1716634345448
+    },
+    {
+        _id: "m",
+        name: "Veggie Delight Pizza",
+        description: "Crunchy vegetables on a thin crust pizza.",
+        price: 140,
+        image: food13,
+        category: "Pizza",
+        date: 1716634345448
+    },
+    {
+        _id: "n",
+        name: "Special Fried Rice With Chicken",
+        description: "Delicious stir-fried rice with tender chicken and spices.",
+        price: 100,
+        image: food15,
+        category: "Rice",
+        date: 1716634345448
+    },
+    {
+        _id: "o",
+        name: "Rice with Side Curries",
+        description: "Steamed rice served with a variety of flavorful side curries.",
+        price: 140,
+        image: food16,
+        category: "Rice",
+        date: 1716634345448
+    },
+    {
+        _id: "p",
+        name: "Jeera Rice",
+        description: "Fragrant basmati rice tempered with cumin seeds.",
+        price: 80,
+        image: food17,
+        category: "Rice",
+        date: 1716634345448
+    },
+];
+
