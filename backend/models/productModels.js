6b461dfb28c497aeaa4cda26d5b28175de9dd445
Taanvi Khevaria <taanvikhevaria@gmail.com>
2025-06-20 01:21:42 +0530

    validating new account

---
+++ backend/models/productModels.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {type:String, required:true},
+    price: {type:Number, required:true},
+    description: {type:String, required:true},
+    image: {type:String, required:true},
+    category: {type:String, required:true},
+    date: {type:Number, required:true}
+})
+
+const productModel = mongoose.models.product || mongoose.model('product', productSchema)
+
+export default productModel
