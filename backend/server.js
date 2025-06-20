3c16eb650488b0a750b3074ca7c016551a53d427
Taanvi Khevaria <taanvikhevaria@gmail.com>
2025-06-20 15:24:25 +0530

    backend connected setup for admin

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -14,6 +15,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
 
 app.get('/', (req,res) => {
     res.send("API Working")
