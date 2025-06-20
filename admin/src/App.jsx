3b1d78fa3488eaac2811a85641753436f810aef8
Taanvi Khevaria <taanvikhevaria@gmail.com>
2025-06-20 20:12:04 +0530

    made sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app-container'>
+      <hr className="app-divider" />
+      <div className="app_content">
+        <Sidebar/>
+        <div className="page-content">
+          <Routes>
+            <Route path='/add' element={<Add />} />
+            <Route path='/list' element={<List />} />
+            <Route path='/orders' element={<Orders />} />
+        </Routes>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { IoMdAddCircleOutline} from 'react-icons/io'
+import {IoLogOut} from 'react-icons/io5'
+import {MdFormatListBulleted, MdAddShoppingCart} from 'react-icons/md'
+import './sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar-container'>
+        <div className="sidebar-header">
+            <h2>FoodSpot</h2>
+        </div>
+        <div className="sidebar-links">
+            <NavLink className='sidebar-link' to='/add'>
+                <IoMdAddCircleOutline className='sidebar-icon' />
+                <p className="sidebar-text">Add Product</p>
+            </NavLink>
+            <NavLink className='sidebar-link' to= '/list'>
+                <MdFormatListBulleted className='sidebar-icon' />
+                <p className="sidebar-text">List Product</p>
+            </NavLink>
+            <NavLink className='sidebar-link' to= '/orders'>
+                <MdAddShoppingCart className='sidebar-icon' />
+                <p className="sidebar-text">Orders</p>
+            </NavLink  > 
+            <button className="sidebar-link loginBtn">
+                <IoLogOut className='sidebar-icon' />
+                <p className="sidebar-text">Logout</p>
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
